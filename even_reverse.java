76aabed0f71d3d09ec6e6b2b999822aaab19a8b6
MeherVenky <71426068+MeherVenky@users.noreply.github.com>
2021-06-28 22:08:25 +0530

    Add files via upload

---
+++ even_reverse.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class even_reverse {
+	
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+	int m=sc.nextInt();
+        int n=sc.nextInt();
+	int i = m;
+
+	while(i>=n) {
+			
+	  if(i%2==0)
+         System.out.print(i+" ");
+	 i--;
+
+       }
+       
+}
+}
